a2a43b7005f96e644c02c3d0b5f40600caea4171
EvLose21 <mobile.roa1@gmail.com>
2022-05-18 12:23:58 +0300

    addtests

---
+++ FridgeProduct.IntegrationTests/ProductsControllerTests.cs
@@ -0,0 +1,98 @@
+﻿using FridgeProduct.Entities;
+using FridgeProduct.Entities.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeProduct.IntegrationTests
+{
+    [TestFixture]
+    public class ProductsControllerTests
+    {
+        [Test]
+        public async Task CheckStatus_SendRequest_ShouldReturnOk()
+        {
+            // Arrange
+
+            WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    var dbContextDescriptor = services.SingleOrDefault(d =>
+                        d.ServiceType == typeof(DbContextOptions<RepositoryContext>));
+
+                    services.Remove(dbContextDescriptor);
+
+                    services.AddDbContext<RepositoryContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("test_db");
+                    });
+                });
+            });
+
+            HttpClient httpClient = webHost.CreateClient();
+
+            // Act
+
+            HttpResponseMessage response = await httpClient.GetAsync("api/products/");
+
+            // Assert
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GetProductsCount_SendRequest_ShouldReturnActualProductsCount()
+        {
+            // Arrange
+
+            WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>().WithWebHostBuilder(builer =>
+            {
+                builer.ConfigureTestServices(services =>
+                {
+                    var dbContextDescriptor = services.SingleOrDefault(d =>
+                        d.ServiceType == typeof(DbContextOptions<RepositoryContext>));
+
+                    services.Remove(dbContextDescriptor);
+
+                    services.AddDbContext<RepositoryContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("test_db");
+                    });
+                });
+            });
+
+            RepositoryContext dbContext =
+                webHost.Services.CreateScope().ServiceProvider.GetService<RepositoryContext>();
+
+            List<Product> products = new() { new Product(), new Product(), new Product() };
+
+            await dbContext.Products.AddRangeAsync(products);
+
+            await dbContext.SaveChangesAsync();
+
+            HttpClient httpClient = webHost.CreateClient();
+
+            // Act
+
+            HttpResponseMessage response = await httpClient.GetAsync("api/delivery/products-count");
+
+            // Assert
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            int productsCount = int.Parse(await response.Content.ReadAsStringAsync());
+
+            Assert.AreEqual(products.Count, productsCount);
+        }
+    }
+}
--- FridgeProduct/Controllers/API/ProductsController.cs
+++ FridgeProduct/Controllers/API/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FridgeProduct.Controllers
@@ -47,6 +48,14 @@ namespace FridgeProduct.Controllers
                 var productDto = _mapper.Map<ProductDto>(product);
                 return Ok(productDto);
             }
+        }
+
+        [HttpGet("products-count")]
+        public IActionResult GetProductCount()
+        {
+            int productsCount = _repository.Product.GetAllProductsQuery(false).Count();
+
+            return Ok(productsCount);
         }
         [HttpPost]
         public async Task<IActionResult> CreateProduct([FromBody] ProductForCreationDto product)
